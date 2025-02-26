475480cecd2836ddf5e974a0a513d562672e39ee
kippel <kippeld@gmail.com>
2025-02-26 08:12:09 +0100

    butifarra

---
+++ src/main.py
@@ -0,0 +1,14 @@
+
+
+class Calculator:
+    def add(self, a: int, b: int) -> int:
+        return a + b
+
+    def subtract(self, a: int, b: int) -> int:
+        return a - b
+
+    def multiply(self, a: int, b: int) -> int:
+        return a * b
+
+    def divide(self, a: int, b: int) -> float:
+        return a / b
